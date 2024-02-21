c4ded96c06587e6db108c788839c4d584a129b41
arrmadillo <dltngks54@gmail.com>
2024-02-21 21:33:00 +0900

    Refactor: 모델,컨트롤러 비동기 적용

--- server/controller/tweet.js
+++ server/controller/tweet.js
@@ -1,17 +1,17 @@
 import * as tweetRepository from '../data/tweet.js';
 
-export function getTweets(req, res) {
+export async function getTweets(req, res) {
   const username = req.query.username;
   //   username 유무에 따라 data값 넘김
-  const data = username
+  const data = await (username
     ? tweetRepository.getAllByUsername(username)
-    : tweetRepository.getAll();
+    : tweetRepository.getAll());
   res.json(data);
 }
 
-export function getTweet(req, res) {
+export async function getTweet(req, res) {
   const id = req.params.id;
-  const tweet = tweetRepository.getById(id);
+  const tweet = await tweetRepository.getById(id);
   if (tweet) {
     res.json(tweet);
   } else {
@@ -19,7 +19,7 @@ export function getTweet(req, res) {
   }
 }
 
-export function createTweet(req, res) {
+export async function createTweet(req, res) {
   // 클라이언트로부터 전달된 데이터
   const { text, name, username } = req.body;
 
@@ -28,16 +28,16 @@ export function createTweet(req, res) {
     return res.status(400).json({ error: '제목과 내용을 모두 입력해주세요.' });
   }
 
-  const newTweet = tweetRepository.create(text, name, username);
+  const newTweet = await tweetRepository.create(text, name, username);
 
   // 성공적인 응답
   return res.status(201).json(newTweet);
 }
 
-export function updateTweet(req, res) {
+export async function updateTweet(req, res) {
   const id = req.params.id;
   const text = req.body.text;
-  const tweet = tweetRepository.update(id, text);
+  const tweet = await tweetRepository.update(id, text);
   if (tweet) {
     res.status(200).json(tweet);
   } else {
@@ -45,8 +45,8 @@ export function updateTweet(req, res) {
   }
 }
 
-export function deleteTweet(req, res) {
+export async function deleteTweet(req, res) {
   const id = req.params.id;
-  tweetRepository.remove(id);
+  await tweetRepository.remove(id);
   res.sendStatus(204);
 }
--- server/data/tweet.js
+++ server/data/tweet.js
@@ -18,19 +18,19 @@ let tweets = [
   },
 ];
 
-export function getAll() {
+export async function getAll() {
   return tweets;
 }
 
-export function getAllByUsername(username) {
+export async function getAllByUsername(username) {
   return tweets.filter((tweet) => tweet.username === username);
 }
 
-export function getById(id) {
+export async function getById(id) {
   return tweets.find((tweet) => tweet.id === id);
 }
 
-export function create(text, name, username) {
+export async function create(text, name, username) {
   // 새로운 트윗 생성
   const newTweet = {
     id: (tweets.length + 1).toString(),
@@ -44,7 +44,7 @@ export function create(text, name, username) {
   return newTweet;
 }
 
-export function update(id, text) {
+export async function update(id, text) {
   const tweet = tweets.find((tweet) => tweet.id === id);
   if (tweet) {
     tweet.text = text;
@@ -53,6 +53,6 @@ export function update(id, text) {
   return tweet;
 }
 
-export function remove(id) {
+export async function remove(id) {
   tweets = tweets.filter((tweet) => tweet.id !== id); // 삭제할 id만 삭제후 재할당
 }
