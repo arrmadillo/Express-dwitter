da52967cfd1ad8aa9d0a4ddf38acedc87e282b02
arrmadillo <dltngks54@gmail.com>
2024-03-01 23:49:32 +0900

    Refactor: 트윗 유효성 검사추가

--- client/src/service/tweet.js
+++ client/src/service/tweet.js
@@ -1,59 +1,32 @@
 export default class TweetService {
-  constructor(baseURL) {
-    this.baseURL = baseURL;
+  constructor(http) {
+    this.http = http;
   }
+
   async getTweets(username) {
-    // 유저네임(쿼리여부)
     const query = username ? `?username=${username}` : '';
-    const response = await fetch(`${this.baseURL}/tweets${query}`, {
+    return this.http.fetch(`/tweets${query}`, {
       method: 'GET',
-      headers: { 'Content-Type': 'application/json' },
     });
-    const data = await response.json();
-    // 200이 아닌경우
-    if (response.status !== 200) {
-      throw new Error(data.message);
-    }
-    return data;
   }
 
   async postTweet(text) {
-    const response = await fetch(`${this.baseURL}/tweets`, {
+    return this.http.fetch(`/tweets`, {
       method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ text, username: 'ellie', name: 'ellie' }),
+      body: JSON.stringify({ text, username: 'ellie', name: 'Ellie' }),
     });
-    const data = await response.json();
-    // 201이 아닌경우
-    if (response.status !== 201) {
-      throw new Error(data.message);
-    }
-    return data;
   }
 
   async deleteTweet(tweetId) {
-    // 유저네임(쿼리여부)
-    const response = await fetch(`${this.baseURL}/tweets/${tweetId}`, {
+    return this.http.fetch(`/tweets/${tweetId}`, {
       method: 'DELETE',
-      headers: { 'Content-Type': 'application/json' },
     });
-    // 204이 아닌경우
-    if (response.status !== 204) {
-      throw new Error();
-    }
   }
 
   async updateTweet(tweetId, text) {
-    const response = await fetch(`${this.baseURL}/tweets/${tweetId}`, {
+    return this.http.fetch(`/tweets/${tweetId}`, {
       method: 'PUT',
-      headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ text }),
     });
-    const data = await response.json();
-    // 200이 아닌경우
-    if (response.status !== 200) {
-      throw new Error(data.message);
-    }
-    return data;
   }
 }
--- server/router/tweets.js
+++ server/router/tweets.js
@@ -1,7 +1,15 @@
 import express from 'express';
+import { body } from 'express-validator';
+// 로컬
 import * as tweetController from '../controller/tweet.js';
+import { validate } from '../middleware/validator.js';
 
 const router = express.Router();
+
+const validateTweet = [
+  body('text').trim().isLength({ min: 2 }).withMessage('3글자 이상 작성!'),
+  validate,
+];
 
 // GET /tweets  /tweets?username=:username
 router.get('/', tweetController.getTweets);
@@ -9,9 +17,9 @@ router.get('/', tweetController.getTweets);
 // GET /tweets/:id
 router.get('/:id', tweetController.getTweet);
 
-router.post('/', tweetController.createTweet);
+router.post('/', validateTweet, tweetController.createTweet);
 
-router.put('/:id', tweetController.updateTweet);
+router.put('/:id', validateTweet, tweetController.updateTweet);
 
 router.delete('/:id', tweetController.deleteTweet);
 
